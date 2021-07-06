78940ad14b90bca6f1c06b6f21e69af2e99edacd
Yauheni Barsuk <barsuck.evgeni@gmail.com>
2021-07-06 12:45:42 +0300

    feat-3: Created user model

--- src/common/entities/user.entity.ts
+++ src/common/entities/user.entity.ts
@@ -2,18 +2,25 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn()
-  id: number
+  @PrimaryGeneratedColumn('uuid')
+  id: string
 
-  @Column({ length: 15 })
-  login: string
+  @Column('varchar', { length: 15, unique: true, nullable: false })
+  name: string
 
-  @Column({ length: 40 })
+  @Column('varchar', { length: 40, nullable: false })
   password: string
 
-  @Column({ length: 30 })
+  @Column('varchar', { length: 30, unique: true, nullable: false })
   email: string
 
-  @Column({ length: 10, nullable: true })
-  role: string
+  @Column('text', { array: true, default: [] })
+  role: string[]
+
+  @Column('bool', { default: false, nullable: false})
+  isBanned: boolean
+
+  @Column('varchar', { nullable: false, default: 0 })
+  profileId: string
+
 }
